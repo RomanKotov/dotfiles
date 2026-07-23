b73080394e773c7a77ee86becd15a253ac6ac053
Roman Kotov <RomanKotov@users.noreply.github.com>
2026-07-23 23:24:59 +0300

    Update treesitter for newer version

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -76,51 +76,9 @@ require("lazy").setup({
       -- Language Support
       {
         "nvim-treesitter/nvim-treesitter",
-        branch = "master",
+        -- TSInstall all - installs all available packages
         build = ":TSUpdate",
-        event = { "VeryLazy" },
-        init = function(plugin)
-          require("lazy.core.loader").add_to_rtp(plugin)
-          require("nvim-treesitter.query_predicates")
-        end,
-        opts_extend = { "ensure_installed" },
-        cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-        opts = {
-          highlight = { enable = true },
-          indent = { enable = true },
-          ensure_installed = {
-            "bash",
-            "c",
-            "diff",
-            "eex",
-            "elixir",
-            "html",
-            "heex",
-            "javascript",
-            "jsdoc",
-            "json",
-            "jsonc",
-            "lua",
-            "luadoc",
-            "luap",
-            "markdown",
-            "markdown_inline",
-            "printf",
-            "python",
-            "query",
-            "regex",
-            "toml",
-            "tsx",
-            "typescript",
-            "vim",
-            "vimdoc",
-            "xml",
-            "yaml",
-          },
-        },
-        config = function(_, opts)
-          require("nvim-treesitter.configs").setup(opts)
-        end,
+        lazy = false,
       },
       -- LSP Support
       {'neovim/nvim-lspconfig'},
